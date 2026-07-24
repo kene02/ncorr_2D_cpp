612c5c9186ba7eae971a7e5dae1b0dcbc9b324ec
Ken Ely <ken.ely.002@gmail.com>
2026-07-24 08:14:28 +0000

    Initial commit

---
+++ build/CMakeFiles/ncorr.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "/workspace/app/lib/libncorr.a"
+)
---
+++ test/bin/CMakeFiles/ncorr_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ncorr_test.dir/link.d"
+  "CMakeFiles/ncorr_test.dir/src/ncorr_test.cpp.o"
+  "CMakeFiles/ncorr_test.dir/src/ncorr_test.cpp.o.d"
+  "ncorr_test"
+  "ncorr_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ncorr_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
