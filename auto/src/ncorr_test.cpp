ff8f3f1a07b93871ce28aac096fed2204b621cd8
Ken Ely <ken.ely.002@gmail.com>
2026-07-26 14:03:17 +0000

    auto - Added try-catch loop for error handling

--- auto/src/ncorr_test.cpp
+++ auto/src/ncorr_test.cpp
@@ -41,92 +41,97 @@ bool analyze_dic_and_strain(const std::string& ref_image_path,
                             const std::string& cur_image_path, 
                             const std::string& roi_image_path,
 							const std::string& output_base) {
-	// Initialize DIC and strain information ---------------//
-	DIC_analysis_input DIC_input;
-	DIC_analysis_output DIC_output;
-	strain_analysis_input strain_input;
-	strain_analysis_output strain_output;
+	try {
+		// Initialize DIC and strain information ---------------//
+		DIC_analysis_input DIC_input;
+		DIC_analysis_output DIC_output;
+		strain_analysis_input strain_input;
+		strain_analysis_output strain_output;
 
-	// Set images
-	std::vector<Image2D> imgs;
-	imgs.push_back(ref_image_path);
-	imgs.push_back(cur_image_path);
-	
-	// Find number of hardware threads
-	unsigned int num_threads = std::thread::hardware_concurrency();
-	if (num_threads == 0)
-    	num_threads = 1;
+		// Set images
+		std::vector<Image2D> imgs;
+		imgs.push_back(ref_image_path);
+		imgs.push_back(cur_image_path);
+		
+		// Find number of hardware threads
+		unsigned int num_threads = std::thread::hardware_concurrency();
+		if (num_threads == 0)
+			num_threads = 1;
 
-	std::cout << "Using " << num_threads << " thread" << (num_threads == 1 ? "" : "s") << "." << std::endl;
-	
-	// Set DIC_input
-	DIC_input = DIC_analysis_input(imgs, 							// Images
-						ROI2D(Image2D(roi_image_path).get_gs() > 0.5),		// ROI
-						3,                                         		// scalefactor
-						INTERP::QUINTIC_BSPLINE_PRECOMPUTE,			// Interpolation
-						SUBREGION::CIRCLE,					// Subregion shape
-						20,                                        		// Subregion radius
-						num_threads,            // # of threads
-						DIC_analysis_config::NO_UPDATE,				// DIC configuration for reference image updates
-						true);							// Debugging enabled/disabled
+		std::cout << "Using " << num_threads << " thread" << (num_threads == 1 ? "" : "s") << "." << std::endl;
+		
+		// Set DIC_input
+		DIC_input = DIC_analysis_input(imgs, 							// Images
+							ROI2D(Image2D(roi_image_path).get_gs() > 0.5),		// ROI
+							3,                                         		// scalefactor
+							INTERP::QUINTIC_BSPLINE_PRECOMPUTE,			// Interpolation
+							SUBREGION::CIRCLE,					// Subregion shape
+							20,                                        		// Subregion radius
+							num_threads,            // # of threads
+							DIC_analysis_config::NO_UPDATE,				// DIC configuration for reference image updates
+							true);							// Debugging enabled/disabled
 
-	// Perform DIC_analysis    
-	DIC_output = DIC_analysis(DIC_input);
+		// Perform DIC_analysis    
+		DIC_output = DIC_analysis(DIC_input);
 
-	// Convert DIC_output to Eulerian perspective
-	DIC_output = change_perspective(DIC_output, INTERP::QUINTIC_BSPLINE_PRECOMPUTE);
+		// Convert DIC_output to Eulerian perspective
+		DIC_output = change_perspective(DIC_output, INTERP::QUINTIC_BSPLINE_PRECOMPUTE);
 
-	// Set units of DIC_output (provide units/pixel)
-	DIC_output = set_units(DIC_output, "mm", 0.2);
+		// Set units of DIC_output (provide units/pixel)
+		DIC_output = set_units(DIC_output, "mm", 0.2);
 
-	// Set strain input
-	strain_input = strain_analysis_input(DIC_input,
-										DIC_output,
-										SUBREGION::CIRCLE,					// Strain subregion shape
-										5);						// Strain subregion radius
-	
-	// Perform strain_analysis
-	strain_output = strain_analysis(strain_input); 	
-        
-    // Get the displacement field you want to access 
-	Disp2D disp = DIC_output.disps.back();  
+		// Set strain input
+		strain_input = strain_analysis_input(DIC_input,
+											DIC_output,
+											SUBREGION::CIRCLE,					// Strain subregion shape
+											5);						// Strain subregion radius
+		
+		// Perform strain_analysis
+		strain_output = strain_analysis(strain_input); 	
+			
+		// Get the displacement field you want to access 
+		Disp2D disp = DIC_output.disps.back();  
 
-	// Get the corresponding v and u Array2Ds 
-	const Array2D<double> &v_array = disp.get_v().get_array();  
-	const Array2D<double> &u_array = disp.get_u().get_array();  
+		// Get the corresponding v and u Array2Ds 
+		const Array2D<double> &v_array = disp.get_v().get_array();  
+		const Array2D<double> &u_array = disp.get_u().get_array();  
 
-	// Get the corresponding ROI2D 
-	ROI2D disp_roi = disp.get_roi(); 
+		// Get the corresponding ROI2D 
+		ROI2D disp_roi = disp.get_roi(); 
 
-	// Get data height and width
-	int height = disp.data_height();
-	int width = disp.data_width();
+		// Get data height and width
+		int height = disp.data_height();
+		int width = disp.data_width();
 
-	// Call the function for both u and v arrays
-	save_to_csv(output_base+"u.csv", u_array, disp_roi, height, width);
-	save_to_csv(output_base+"v.csv", v_array, disp_roi, height, width);
+		// Call the function for both u and v arrays
+		save_to_csv(output_base+"u.csv", u_array, disp_roi, height, width);
+		save_to_csv(output_base+"v.csv", v_array, disp_roi, height, width);
 
-	// Get the strain field you want to access 
-	Strain2D strain = strain_output.strains.back();  
+		// Get the strain field you want to access 
+		Strain2D strain = strain_output.strains.back();  
 
-	// Get the corresponding eyy, exy, and exx Array2Ds 
-	const Array2D<double> &eyy_array = strain.get_eyy().get_array();  
-	const Array2D<double> &exy_array = strain.get_exy().get_array();  
-	const Array2D<double> &exx_array = strain.get_exx().get_array(); 
+		// Get the corresponding eyy, exy, and exx Array2Ds 
+		const Array2D<double> &eyy_array = strain.get_eyy().get_array();  
+		const Array2D<double> &exy_array = strain.get_exy().get_array();  
+		const Array2D<double> &exx_array = strain.get_exx().get_array(); 
 
-	// Get the corresponding ROI2D 
-	ROI2D strain_roi = strain.get_roi();  
-	
-	// Get data height and width
-	height = strain.data_height();
-	width = strain.data_width();
+		// Get the corresponding ROI2D 
+		ROI2D strain_roi = strain.get_roi();  
+		
+		// Get data height and width
+		height = strain.data_height();
+		width = strain.data_width();
 
-	// Call the function for eyy, exy, and exx arrays
-	save_to_csv(output_base+"eyy.csv", eyy_array, strain_roi, height, width);
-	save_to_csv(output_base+"exy.csv", exy_array, strain_roi, height, width);
-	save_to_csv(output_base+"exx.csv", exx_array, strain_roi, height, width);
+		// Call the function for eyy, exy, and exx arrays
+		save_to_csv(output_base+"eyy.csv", eyy_array, strain_roi, height, width);
+		save_to_csv(output_base+"exy.csv", exy_array, strain_roi, height, width);
+		save_to_csv(output_base+"exx.csv", exx_array, strain_roi, height, width);
 
-  	return true;
+		return true;
+	} catch (const std::exception& e) {
+		std::cerr << "DIC analysis failed: " << e.what() << std::endl;
+    	return false;
+	}
 }
 
 int main() {
